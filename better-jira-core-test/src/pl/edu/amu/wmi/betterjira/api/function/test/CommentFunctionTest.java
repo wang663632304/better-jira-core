e97f9ddfb6b5b1da105e96d7609b65a5d5af6fd3
Dawid Jewko <dj18125@st.amu.edu.pl>
2013-05-26 12:46:57 +0200

    Comments test

--- better-jira-core-test/src/pl/edu/amu/wmi/betterjira/api/function/test/CommentFunctionTest.java
+++ better-jira-core-test/src/pl/edu/amu/wmi/betterjira/api/function/test/CommentFunctionTest.java
@@ -2,10 +2,8 @@ package pl.edu.amu.wmi.betterjira.api.function.test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.TimeZone;
 
 import pl.edu.amu.wmi.betterjira.api.ServerConnector;
 import pl.edu.amu.wmi.betterjira.api.function.BasicAuthentication;
@@ -20,58 +18,64 @@ import pl.edu.amu.wmi.betterjira.utils.UserInfo;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class CommentFunctionTest extends
-ActivityInstrumentationTestCase2<TestActivity>{
+		ActivityInstrumentationTestCase2<TestActivity> {
 
-    public CommentFunctionTest() {
-	super(TestActivity.class.getPackage().toString(), TestActivity.class);
-    }
-    
-    public void testGetAllComments() throws MalformedURLException, LoginException, BadResponse{
-	ServerConnector.setServerURL(new URL("https://jira.wmi.amu.edu.pl/"));
+	public CommentFunctionTest() {
+		super(TestActivity.class.getPackage().toString(), TestActivity.class);
+	}
 
-	BasicAuthentication basic = new BasicAuthentication();
-	Session session = basic.login(UserInfo.login, UserInfo.password);
+	public void testGetAllComments() throws MalformedURLException,
+			LoginException, BadResponse {
+		ServerConnector.setServerURL(new URL("https://jira.wmi.amu.edu.pl/"));
 
-	CommentFunction commentFunction = new CommentFunction(session, "BET-36");
-	assertNotNull(commentFunction);
-	CommentsList allComments = commentFunction.getAllComments();
-	assertNotNull(allComments);
-	assertTrue(allComments.getMaxResults()>0);
-	assertTrue(allComments.getTotal()>0);
-	
-	
-	
-    }
-    
-    public void testSendComment() throws MalformedURLException, LoginException, BadResponse{
-    	ServerConnector.setServerURL(new URL("https://jira.wmi.amu.edu.pl/"));
+		BasicAuthentication basic = new BasicAuthentication();
+		Session session = basic.login(UserInfo.login, UserInfo.password);
 
-    	BasicAuthentication basic = new BasicAuthentication();
-    	Session session = basic.login(UserInfo.login, UserInfo.password);
-    	//BET-13 to jest task do testów
-    	CommentFunction commentFunction = new CommentFunction(session, "BET-13");
-    	//Minuta przed wysłaniem
-    	Date dateBefore=new Date();
-    	dateBefore.setTime(dateBefore.getTime()-60*1000);
-    	Comment comment1 = commentFunction.sendComment("test1");
-    	//Minuta po wysłaniu
-    	Date dateAfter=new Date();
-    	dateAfter.setTime(dateAfter.getTime()+60*1000);
-    	assertNotNull(comment1);
-    	assertEquals("test1", comment1.getBody());
-    	assertEquals(UserInfo.login, comment1.getAuthor().getName());
-    	assertEquals(UserInfo.login, comment1.getUpdateAuthor().getName());
-    	
-    	
-//    	System.out.println("Created: "+comment1.getCreated());
-//    	System.out.println("dateBefore: "+dateBefore);
-//    	System.out.println("dateAfter: "+dateAfter);
-    	assertTrue(comment1.getCreated().after(dateBefore));
-    	assertTrue(comment1.getCreated().before(dateAfter));
-    	assertTrue(comment1.getUpdated().after(dateBefore));
-    	assertTrue(comment1.getUpdated().before(dateAfter));
-    	
-    	
-    }
+		CommentFunction commentFunction = new CommentFunction(session, "BET-13");
+		assertNotNull(commentFunction);
+		CommentsList allComments = commentFunction.getAllComments();
+		assertNotNull(allComments);
+		assertTrue(allComments.getMaxResults() > 0);
+		assertTrue(allComments.getTotal() > 0);
+		ArrayList<Comment> comments = allComments.getComments();
+		assertNotNull(comments);
+		for(Comment comm : comments){
+			assertNotNull(comm);
+			
+		}
+		assertEquals("Marcin Skibicki", comments.get(0).getAuthor().getDisplayName());
+		assertEquals("umgnt", comments.get(0).getBody());
+
+	}
+
+	public void testSendComment() throws MalformedURLException, LoginException,
+			BadResponse {
+		ServerConnector.setServerURL(new URL("https://jira.wmi.amu.edu.pl/"));
+
+		BasicAuthentication basic = new BasicAuthentication();
+		Session session = basic.login(UserInfo.login, UserInfo.password);
+		// BET-13 to jest task do testów
+		CommentFunction commentFunction = new CommentFunction(session, "BET-13");
+		// Minuta przed wysłaniem
+		Date dateBefore = new Date();
+		dateBefore.setTime(dateBefore.getTime() - 60 * 1000);
+		Comment comment1 = commentFunction.sendComment("test1");
+		// Minuta po wysłaniu
+		Date dateAfter = new Date();
+		dateAfter.setTime(dateAfter.getTime() + 60 * 1000);
+		assertNotNull(comment1);
+		assertEquals("test1", comment1.getBody());
+		assertEquals(UserInfo.login, comment1.getAuthor().getName());
+		assertEquals(UserInfo.login, comment1.getUpdateAuthor().getName());
+
+		System.out.println("Created: " + comment1.getCreated());
+		System.out.println("dateBefore: " + dateBefore);
+		System.out.println("dateAfter: " + dateAfter);
+		assertTrue(comment1.getCreated().after(dateBefore));
+		assertTrue(comment1.getCreated().before(dateAfter));
+		assertTrue(comment1.getUpdated().after(dateBefore));
+		assertTrue(comment1.getUpdated().before(dateAfter));
+
+	}
 
 }
