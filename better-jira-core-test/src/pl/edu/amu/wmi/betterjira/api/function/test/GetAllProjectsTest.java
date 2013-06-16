b1311ef1bd29075faa70fdeda2594c037539f6a4
Dawid Jewko <dj18125@st.amu.edu.pl>
2013-06-16 18:23:15 +0200

    BET-68 Mock server for Get all projects test.

--- better-jira-core-test/src/pl/edu/amu/wmi/betterjira/api/function/test/GetAllProjectsTest.java
+++ better-jira-core-test/src/pl/edu/amu/wmi/betterjira/api/function/test/GetAllProjectsTest.java
@@ -1,7 +1,9 @@
 package pl.edu.amu.wmi.betterjira.api.function.test;
 
-import java.net.URL;
 import java.util.ArrayList;
+
+import com.google.mockwebserver.MockResponse;
+import com.google.mockwebserver.MockWebServer;
 
 import pl.edu.amu.wmi.betterjira.api.ServerConnector;
 import pl.edu.amu.wmi.betterjira.api.function.BasicAuthentication;
@@ -11,7 +13,6 @@ import pl.edu.amu.wmi.betterjira.api.function.data.Session;
 import pl.edu.amu.wmi.betterjira.api.function.data.test.TestActivity;
 import pl.edu.amu.wmi.betterjira.utils.UserInfo;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 
 public class GetAllProjectsTest extends
 		ActivityInstrumentationTestCase2<TestActivity> {
@@ -20,7 +21,13 @@ public class GetAllProjectsTest extends
 	}
 
 	public void testGetAllVisibleProjects() throws Exception {
-		ServerConnector.setServerURL(new URL("https://jira.wmi.amu.edu.pl/"));
+		MockWebServer server = new MockWebServer();
+		server.enqueue(new MockResponse()
+				.setBody("{\"session\":{\"value\":\"4192C9DD35545EFFDAA9EAE196AFAC41\",\"name\":\"JSESSIONID\"},\"loginInfo\":{\"previousLoginTime\":\"2013-06-07T10:13:55.759+0200\",\"loginCount\":184}}"));
+		server.enqueue(new MockResponse()
+				.setBody("[{\"self\":\"https://jira.wmi.amu.edu.pl/rest/api/2/project/BET\",\"id\":\"10018\",\"key\":\"BET\",\"name\":\"BetterJira\",\"avatarUrls\":{\"16x16\":\"https://jira.wmi.amu.edu.pl/secure/projectavatar?size=small&pid=10018&avatarId=10002\",\"48x48\":\"https://jira.wmi.amu.edu.pl/secure/projectavatar?pid=10018&avatarId=10002\"}}]"));
+		server.play();
+		ServerConnector.setServerURL(server.getUrl("/"));
 
 		BasicAuthentication basic = new BasicAuthentication();
 		Session session = basic.login(UserInfo.login, UserInfo.password);
@@ -29,11 +36,11 @@ public class GetAllProjectsTest extends
 		GetAllProjects getAllProjects = new GetAllProjects(session);
 		ArrayList<Project> projects = getAllProjects.getAllVisibleProjects();
 		assertNotNull(projects);
-		Log.d("projekty", projects.toString());
 		assertEquals(1, projects.size());
 		assertNotNull(projects.get(0));
 		assertEquals("BetterJira", projects.get(0).getName());
 		assertEquals("BET", projects.get(0).getKey());
+		assertEquals(10018, projects.get(0).getId());
 
 	}
 }
